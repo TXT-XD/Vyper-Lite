6f4be0ae2bd2df856f5a6d80f1b285d08b9377f0
TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>
2024-09-08 07:07:16 +0600

    Update Vyper.py

--- Vyper.py
+++ Vyper.py
@@ -2,8 +2,7 @@ import os, sys, platform
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system(f"mv no.cpython-311.so {sys.path[2]}/no.cpython-311.so")
-    os.system("chmod +x test")
-    os.system("./test")
+    import test
 else:
     #os.system(f"mv no32.cpython-311.so {sys.path[2]}/no32.cpython-311.so")
     #import test33
