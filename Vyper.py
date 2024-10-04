9ca5582af032e0503011448276e0bce3bca2a74c
TANIM HOSSAIN <145688561+TXT-XD@users.noreply.github.com>
2024-10-04 20:50:20 +0600

    Update Vyper.py

--- Vyper.py
+++ Vyper.py
@@ -1,8 +1,8 @@
 import os, sys, platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    os.system(f"mv no.cpython-311.so {sys.path[2]}/no.cpython-311.so")
-    import test
+    os.system(f"mv _vyper.cpython-311.so {sys.path[2]}/_vyper.cpython-311.so")
+    import xnx
 else:
     #os.system(f"mv no32.cpython-311.so {sys.path[2]}/no32.cpython-311.so")
     #import test33
